98729b51b9dd5d2b47d8dc485ddba1d5ade75223
sjaehn <kontakt@jahnichen.de>
2021-04-02 22:18:40 +0200

    Typo

--- src/Locale_IT.hpp
+++ src/Locale_IT.hpp
@@ -3,7 +3,7 @@
 #define BSLIZR_LABEL_MONITOR "Monitor"
 #define BSLIZR_LABEL_ATTACK "Attacco"
 #define BSLIZR_LABEL_RELEASE "Rilascio"
-#define BSLIZR_LABEL_SEQUENCES_PER_BAR "Sequenza per misura"
+#define BSLIZR_LABEL_SEQUENCES_PER_BAR "Sequenze per misura"
 #define BSLIZR_LABEL_NUMBER_OF_STEPS "Numero di passi"
 #define BSLIZR_LABEL_STEP_SHAPE "Forma del passo"
 #define BSLIZR_LABEL_SEQUENCE_MONITOR "Monitor della sequenza"
