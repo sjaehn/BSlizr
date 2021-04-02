107ca6d0b96f1c93d5b799a4f1a3c6aa754fc1f7
sjaehn <kontakt@jahnichen.de>
2021-04-02 22:10:38 +0200

    Add IT locale by LAM

--- README.md
+++ README.md
@@ -110,7 +110,7 @@ and edit the parameters. You can also provide a background image at `./surface_S
 
 ## What's new
 
-* Locales: DE
+* Locales: DE, IT
 
 Links
 -----
---
+++ src/Locale_IT.hpp
@@ -0,0 +1,10 @@
+// Language pack kindly provided by LAM
+
+#define BSLIZR_LABEL_MONITOR "Monitor"
+#define BSLIZR_LABEL_ATTACK "Attacco"
+#define BSLIZR_LABEL_RELEASE "Rilascio"
+#define BSLIZR_LABEL_SEQUENCES_PER_BAR "Sequenza per misura"
+#define BSLIZR_LABEL_NUMBER_OF_STEPS "Numero di passi"
+#define BSLIZR_LABEL_STEP_SHAPE "Forma del passo"
+#define BSLIZR_LABEL_SEQUENCE_MONITOR "Monitor della sequenza"
+#define BSLIZR_LABEL_JACK_STOP "Trasporto Jack fermo o arrestato."
